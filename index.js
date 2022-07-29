d2bb21db136643585b7438d1131f4081711c3297
kasoqian <95743792+kasoqian@users.noreply.github.com>
2022-07-29 16:07:47 +0800

    feat: 新增metadata

--- index.js
+++ index.js
@@ -5,49 +5,6 @@ const beginTokenID = 10;
 const baseURI =
   "https://raw.githubusercontent.com/W3T-Web3-Genius-Tools/sbt/main/src/metadata/images/";
 
-const metaInfo = [
-  {
-    name: "七套房",
-    attributes: { 房子: "七套" },
-  },
-];
-
-const metaBase = {
-  name: "Rookie–Badge of Navigators #0",
-  description:
-    "The brand new world of Web3 is just like an ocean, where the waves are overwhelming while the views are magnificent. Anyone who would like to take part in it is a worrier.  Welcome, Navigators!",
-  image:
-    "https://raw.githubusercontent.com/W3T-Web3-Genius-Tools/sbt/main/src/metadata/images/0.png",
-  attributes: [],
-};
-
-let arr = new Array(10).fill({ name: "周" });
-
-async function readfile() {
-  let result;
-
-  return result;
-}
-
-function main() {
-  arr.forEach(async (item, index) => {
-    let metadataOnce = {};
-    let tokenID = index + beginTokenID;
-
-    metadataOnce = {
-      name: `${item.name} #${tokenID}`,
-    };
-
-    metadataOnce.image = `${baseURI}${tokenID}.jpg`;
-    metadataOnce.description = "周劼的精彩人生";
-
-    fs.writeFileSync(
-      path.join(__dirname, `./metadata/json/${index + beginTokenID}.json`),
-      JSON.stringify(metadataOnce, null, 2),
-    );
-  });
-}
-
 const meatadatas = () => {
   return new Promise(async (res, rej) => {
     let data = await fs.readFileSync(
